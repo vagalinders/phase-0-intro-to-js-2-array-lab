a120a992b6ebcf06692a1bc06660db857ce26e9c
Chris Mann <chrispmann@Thinkpad-Senbei.localdomain>
2022-08-12 11:22:04 -0700

    commit tree efe4d1b0c41dca39734828d258a0375172cd3fb3

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(cat){
+    cats.push(cat);
+}
+
+function destructivelyPrependCat(cat){
+    cats.unshift(cat);
+}
+
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function appendCat(cat){
+    const moreCats = [...cats, cat];
+    return moreCats;
+}
+
+function prependCat(cat){
+    const moreCats = [cat, ...cats];
+    return moreCats;
+}
+
+function removeLastCat(){
+    const lessCats = cats.slice(0, -1);
+    return lessCats;
+}
+
+function removeFirstCat(){
+    const lessCats = cats.slice(1);
+    return lessCats;
+}
